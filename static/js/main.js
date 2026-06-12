41341a9e980425e333be4d5e9ef28f7337914a07
mgroupsinfraprojects <mgroupsinfraprojects@gmail.com>
2026-06-12 20:57:18 +0530

    Delete static directory

--- static/js/main.js
+++
@@ -1,5 +0,0 @@
-const toggle = document.querySelector('[data-nav-toggle]');
-const nav = document.querySelector('[data-nav]');
-if (toggle && nav) {
-  toggle.addEventListener('click', () => nav.classList.toggle('open'));
-}
